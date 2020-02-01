d5e34cf1994e578d5d930449070d4047e27e8a63
Anmol Arora <anmolarora1711@gmail.com>
2020-02-01 14:02:40 +0530

    Notes

---
+++ A.java
@@ -0,0 +1,18 @@
+java C
+B.main()
+class B{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+} 
+class C{
+    
+}
+class Student{
+    static void getDetails(){
+
+    }
+}
+// Student studentObject = new Student();
+// studentObject.getDetails();
+Student.getDetails();
